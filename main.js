d8d443f61d301f5289559dcd0ce1afec82b8d8f5
ReeceDonovan <reecedonovan99@gmail.com>
2020-09-11 08:49:43 +0100

    Intial Commit - Building basis of maze generation

---
+++ main.js
@@ -0,0 +1,72 @@
+let maze = document.querySelector(".maze");
+let ctx = maze.getContext("2d");
+
+let current;
+
+class Maze {
+    constructor(size, rows, cols) {
+        this.size = size;
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = [];
+        this.stack = [];
+    }
+
+    setup() {
+        for (let r = 0; r < this.rows; r++) {
+            let row = [];
+            for (let c = 0; c < this.cols; c++) {
+                let cell = new Cell(r, c, this.grid, this.size);
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
+        current = this.grid[0][0];
+    }
+}
+
+class Cell {
+    constructor(rowNum, colNum, parentGrid, parentSize) {
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.parentGrid = parentGrid;
+        this.parentSize = parentSize;
+        this.visited = false;
+        this.walls = {
+            topWall: true,
+            bottomWall: true,
+            leftWall: true,
+            rightWall: true,
+        };
+    }
+}
+
+
+let newMaze = new Maze(500, 10, 10);
+newMaze.setup();
+
+//
+//
+//
+//
+//
+// var cols = 5;
+// var rows = 5;
+// var grid = new Array(cols);
+
+// function setup() {
+//     createCanvas(400, 400);
+//     console.log('A*');
+
+//     //2D Array
+//     for (var i = 0; i < cols; i++) {
+//         grid[i] = new Array(rows);
+//     }
+
+// }
+
+// function draw() {
+//     background(0);
+
+
+// }
